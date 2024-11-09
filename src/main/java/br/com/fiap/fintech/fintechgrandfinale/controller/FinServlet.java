6d0a0170cebf3ce79aea2cf55d346f2a34aaacea
Allef Rodrigues <allef.rodri@gmail.com>
2024-11-09 13:17:30 -0300

    correcao servlet

--- src/main/java/br/com/fiap/fintech/fintechgrandfinale/controller/FinServlet.java
+++ src/main/java/br/com/fiap/fintech/fintechgrandfinale/controller/FinServlet.java
@@ -4,7 +4,7 @@ import java.io.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "Teste", value = "asd")
+@WebServlet(name = "Teste", value = "/asd")
 public class FinServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
