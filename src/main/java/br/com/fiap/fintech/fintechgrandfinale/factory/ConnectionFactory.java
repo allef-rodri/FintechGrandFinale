3504b7757a25626265e9aebc8ea68a3f11a9fec7
Allef Rodrigues <allef.rodri@gmail.com>
2024-11-09 19:14:35 -0300

    Correção conexão com DB

--- src/main/java/br/com/fiap/fintech/fintechgrandfinale/factory/ConnectionFactory.java
+++ src/main/java/br/com/fiap/fintech/fintechgrandfinale/factory/ConnectionFactory.java
@@ -1,16 +1,31 @@
 package br.com.fiap.fintech.fintechgrandfinale.factory;
 
+import java.net.URI;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
 
+    private static Connection connection = null;
     private static final String url = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
     private static final String usuario = "RM556212";
     private static final String senha = "Diego#2024";
 
-    public static Connection getConnection() throws SQLException{
-        return DriverManager.getConnection(url, usuario, senha);
+    public static Connection getConnection() throws SQLException {
+
+        try{
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            connection = DriverManager.getConnection(url, usuario, senha);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+
     }
 }
